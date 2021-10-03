6926d2802ef20bf23f014de09eaddfa9af58b68c
developer-suhag <developersuhag@gmail.com>
2021-10-03 16:45:59 +0600

    home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,20 @@
+import { Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import "./Home.css";
+
+import checkList from "../../images/Checklist-bro.png";
 
 const Home = () => {
   return (
-    <div>
-      <h2>Welcome home</h2>
-    </div>
+    <Container>
+      <div className="checklist">
+        <Typography variant="h3" component="h3">
+          To do list
+        </Typography>
+        <img src={checkList} alt="" />
+      </div>
+    </Container>
   );
 };
 
